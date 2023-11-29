54ccfab321a3910c0c4ee1e0c85d1465456d6515
juancruza46 <juan.cruza46@gmail.com>
2023-11-29 09:36:06 -0500

    this will add image as well as js layout

---
+++ script.js
@@ -0,0 +1,31 @@
+//start menu 
+
+//prompt user for a name 
+
+//start ganme button/hide event listener
+
+//Card amounts will be: 12
+
+//Cards will be randomly shuffled
+
+//Cards will remain faced down until clicked upon event listeer
+
+//When clicked cards will be revealed ( 2 at a time)
+
+//if a match = remove cards
+
+//if not a match, flip over again, prmpt user to try again
+
+//the player will have a total of 5 errors
+
+//if 5 errors threshold is reached, display : game over
+
+//display you lost 
+//new game button
+
+//if all cards have been matched, display : game over you won
+//new game button
+
+//create a timer of 5 minutes
+//if 5 minutes is reached and cards have not been matched, despite amount of errors display game over you lose 
+//new game button
