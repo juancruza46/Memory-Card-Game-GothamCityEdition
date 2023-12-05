52fbfff906b4edff2e03b10ed7acc6df05596678
juancruza46 <juan.cruza46@gmail.com>
2023-12-05 11:31:12 -0500

    adds a you win function

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-//start ganme button/hide event listene
-//Select a card addEventListener("click", handleMove)
 //declare state variables:
 let firstCard= null;
 let secondCard= null;
 
-//Create a constant message display
+//Create a constant message display 
 const message = document.querySelector('h4');
 
-//flip the cards over
+//add event listeners when you select a card
 function flip(e) {
   const selectedCard = e.currentTarget;
   selectedCard.className = selectedCard.className.replace('backCard', '').trim();
@@ -18,34 +16,51 @@ function flip(e) {
   } else if (secondCard === null && selectedCard !== firstCard) {
     secondCard = selectedCard;
     if (firstCard.className === secondCard.className) {
+     
       //if cards match turn white; hide
       firstCard.style.backgroundColor = 'white';
       secondCard.style.backgroundColor = 'white';
+      //Clear cards chosen
       firstCard = null;
       secondCard = null;
       message.innerHTML = 'You Got A Match!';
+
+      //if all cards match end game
+      let allCards = document.querySelectorAll('.slot');
+      let allMatched = true;
+      allCards.forEach(card => {
+        if (!card.style.backgroundColor || card.style.backgroundColor !== 'white') {
+          allMatched = false;
+        }
+      });
+      if (allMatched) {
+        clearInterval(interval);
+        message.innerHTML = 'You Win!';
+      }
     } else {
+      //stop the clock
       setTimeout(() => {
+        
         //if cardss do not match flip back to origin state
         firstCard.className += ' backCard';
         secondCard.className += ' backCard';
         firstCard = null;
         secondCard = null;
-        message.innerHTML = 'Not a natch!';
+        message.innerHTML = 'Not a match!';
       }, 200);
     }
   }
 }
-
-    let timer = 50;//or 3 min
-    let countDown = document.querySelector('h3');//assign to h3
-    //function for countdown, make sure it works before adding to top function
+//Create a timer, if it runs out = you lose
+    let timer = 50;
+    let countDown = document.querySelector('h3');
     let interval = setInterval(() => {
         timer--;
         countDown.textContent = `Timer: ${timer}`;
     if (timer <= 0) {
         clearInterval(interval);
         message.innerHTML = 'Out of Time!';
+      
   }
 }, 1000);
 
