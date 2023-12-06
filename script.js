ccdf5cd5a12102514cfb9f4b1309b7b4623cd94c
juancruza46 <juan.cruza46@gmail.com>
2023-12-06 15:40:35 -0500

    timer message, attempts count

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //declare state variables:
 let firstCard= null;
 let secondCard= null;
+let remainingAttempts = 5;
+let allCards = document.querySelectorAll('.slot');
+//randomize deck
+
 
 //Create a constant message display 
 const message = document.querySelector('h2');
@@ -28,7 +32,6 @@ if (firstCard === null) {
       message.innerHTML = 'You Got A Match!';
 
       //if all cards match end game
-      let allCards = document.querySelectorAll('.slot');
       let allMatched = true;
       allCards.forEach(card => {
         if (!card.style.backgroundColor || card.style.backgroundColor !== 'white') {
@@ -47,6 +50,12 @@ if (firstCard === null) {
         secondCard.className += ' backCard';
         firstCard = null;
         secondCard = null;
+         remainingAttempts --;
+         document.querySelector('h4').textContent = `Attempt(s): ${remainingAttempts}`;
+         if (remainingAttempts <= 0) {
+           clearInterval(interval);
+           message.innerHTML = 'OUT OF LIVES! JOKER WINS!';
+         }
         message.innerHTML = 'Not a match!';
 
       }, 200);
@@ -58,10 +67,10 @@ if (firstCard === null) {
     let countDown = document.querySelector('h3');
     let interval = setInterval(() => {
         timer--;
-        countDown.textContent = `Timer: ${timer}`;
+        countDown.textContent = `Bomb Timer: ${timer}`;
     if (timer <= 0) {
         clearInterval(interval);
-        message.innerHTML = 'Out of Time! Joker Wins !';
+        message.innerHTML = 'The Bomb Exploded! Joker Wins!';
       
   }
 }, 1000);
@@ -72,7 +81,6 @@ newGameButton.addEventListener("click", () => {
   //testing button funtionality ; FIX LATER
   location.reload();
 });
-
 
 
 
